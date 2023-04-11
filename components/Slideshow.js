3adf69655242682934e41cd92e5fa2d721f4840b
admirdemir <admirdemir@gmail.com>
2023-04-11 23:55:41 +0200

    Slideshow component added

---
+++ components/Slideshow.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Zoom } from "react-slideshow-image";
+import "react-slideshow-image/dist/style.css";
+
+const Slideshow = () => {
+  const images = [
+    "images/images1.jpg",
+    "images/images2.jpg",
+    "images/images3.jpg",
+    "images/images4.jpg",
+    "images/images5.jpg",
+  ];
+};
+
+export default Slideshow;
---
+++ pages/herosection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SlideShow from "../components/Slideshow";
+
+function herosection() {
+  return (
+    <div>
+      <SlideShow />
+    </div>
+  );
+}
+
+export default herosection;
